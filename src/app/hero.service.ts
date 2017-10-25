00d3df8755ff49b8db2f46fee3ac5c2fd1ac8345
Dominic Bure <dominicb84@gmail.com>
2017-10-25 15:26:22 +0200

    added Promise-based data-service

---
+++ src/app/hero.service.ts
@@ -0,0 +1,10 @@
+import { HEROES } from './mock-heroes';
+import { Hero } from './hero';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class HeroService {
+    getHeroes(): Promise<Hero[]> {
+        return Promise.resolve(HEROES);
+      }
+}
